b37da86723f40fac147cda189804d7a3ec3282de
Babu Kumar <babukumarbandela@gmail.com>
2024-10-21 09:18:43 +0530

    Prime number or not in Python language

---
+++ prime_or_not.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a prime number: "))
+flag = False
+if num<=1:
+    print("{0} is not a prime number".format(num))
+elif num>1:
+    for i in range(2,num):
+        if (num%i)==0:
+            flag = True
+            break
+
+    if flag:
+        print("{0} is not a prime number".format(num))
+    else:
+        print("{0} is a prime number".format(num))
+
